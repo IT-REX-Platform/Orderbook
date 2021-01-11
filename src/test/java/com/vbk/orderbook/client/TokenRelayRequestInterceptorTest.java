3b42452d5c5db6df10ba771293e7d4aff4ce5818
GarondEisenfaust <chr.schr@web.de>
2021-01-11 12:02:13 +0100

    Added test to test test coverage

---
+++ src/test/java/com/vbk/orderbook/client/TokenRelayRequestInterceptorTest.java
@@ -0,0 +1,14 @@
+package com.vbk.orderbook.client;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenRelayRequestInterceptorTest {
+
+    @Test
+    void apply() {
+        assertTrue(true);
+    }
+}
